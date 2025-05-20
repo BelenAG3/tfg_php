a2fa95048381ebdfbe58d7e2246414142c1c4d8a
BelenAG3 <belen_aparicio_m-tsapp1ma2324@cev.com>
2025-05-20 10:16:05 +0200

    first commit

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*$Servidor = "127.0.0.1";
+	$Usuario = "root";
+	$Password = "";
+	$bd = "papeleria";*/
+
+	$Servidor = "sql206.infinityfree.com";
+	$Usuario = "if0_39021209";
+	$Password = "2sL5MavNHLU037";
+	$bd = "if0_39021209_pawffe";
+  
+?>
